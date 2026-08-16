8f0187dd8e51f40b72777a9d2c945862c1f82967
ORIONFINANCE-admin <oriongroup.admin@gmail.com>
2026-08-16 01:05:23 -0300

    Add files via upload

---
+++ mobile/service-worker.js
@@ -0,0 +1,5 @@
+const CACHE="orion-mobile-v10-1-retired";
+self.addEventListener("install",event=>{self.skipWaiting();});
+self.addEventListener("activate",event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.registration.unregister()).then(()=>self.clients.claim()));});
+self.addEventListener("fetch",()=>{});
+self.addEventListener("message",event=>{if(event.data?.type==="SKIP_WAITING")self.skipWaiting();});
