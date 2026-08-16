02e6ede0c2ebb0870484ed151d6488c37a7ecbeb
ORIONFINANCE-admin <oriongroup.admin@gmail.com>
2026-08-16 01:05:06 -0300

    Delete mobile directory

--- mobile/service-worker.js
+++
@@ -1,5 +0,0 @@
-const CACHE="orion-mobile-v10-0-retired";
-self.addEventListener("install",event=>{self.skipWaiting();});
-self.addEventListener("activate",event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.registration.unregister()).then(()=>self.clients.claim()));});
-self.addEventListener("fetch",()=>{});
-self.addEventListener("message",event=>{if(event.data?.type==="SKIP_WAITING")self.skipWaiting();});
